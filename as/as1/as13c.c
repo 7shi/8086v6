8f4c443403eb5b9fda4dcbd1efc1a8e3ac701115
7shi <7shi@live.jp>
2014-01-02 00:05:26 +0900

    [as1]struct opcodeを追加

--- as/as1/as13c.c
+++ as/as1/as13c.c
@@ -6,6 +6,8 @@ fbcheck: as13.s, as14.s
 checkeos: as13.s
 */
 
+struct opcode { char op0, op1; int op2; };
+
 char *savop;
 int ifflg;
 int line;
@@ -13,27 +15,20 @@ int numval;
 int dotrel;
 int dot;
 char fbfil;
-char nxtfb[];
+struct opcode nxtfb;
 char curfbr[];
 int curfb[];
 
-struct { int ival; };
-
 assem() {
-	int r0;
-	char *r1;
-	int r2;
-	int r3;
-	char *r4;
-	char *oldr4;
+	int r0, r1, r2, r3, r4, oldr4;
 	for (;;) {
 		r4 = readop();
 		if (!checkeos(r4)) {
 			if (ifflg) {
 				if (r4 > 0200) {
-					if (*r4 == 021/*if*/)
+					if (r4->op0 == 021/*if*/)
 						++ifflg;
-					else if (*r4 == 022/*endif*/)
+					else if (r4->op0 == 022/*endif*/)
 						--ifflg;
 				}
 				continue;
@@ -46,11 +41,10 @@ assem() {
 				if (r1 < 0200)
 					error("x");
 				else if (r1 != &dotrel || (r3 & ~040) == dotrel) {
-					*r1 =& ~037;
-					r3 =& 37;
-					if (!r3) r2 = 0;
-					*r1 =| r3;
-					(r1 + 2)->ival = r2;
+					r3 =& 037;
+					r1->op0 =& ~037;
+					r1->op0 =| r3;
+					r1->op2 = r3 ? r2 : 0;
 				} else {
 					error(".");
 					dotrel = 2;
@@ -58,17 +52,17 @@ assem() {
 			} else if (r4 == ':') {
 				r4 = oldr4;
 				if (r4 >= 0200) {
-					if (*r4 & 037) error("m");
-					*r4 =| dotrel;
-					(r4 + 2)->ival = dot;
+					if (r4->op0 & 037) error("m");
+					r4->op0 =| dotrel;
+					r4->op2 = dot;
 				} else if (r4 == 1/*digit*/) {
 					r0 = fbcheck(numval);
 					curfbr[r0] = dotrel;
 					curfb[r0] = dot;
-					nxtfb[0] = dotrel;
-					nxtfb[1] = r0 << 1;
-					(nxtfb + 2)->ival = dot;
-					write(fbfil, nxtfb, 4);
+					nxtfb.op0 = dotrel;
+					nxtfb.op1 = r0 << 1;
+					nxtfb.op2 = dot;
+					write(fbfil, &nxtfb, 4);
 				} else
 					error("x");
 				continue;
