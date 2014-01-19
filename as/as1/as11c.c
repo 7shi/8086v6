949b5834f25b8683b236e5ca6ffa449de78bb447
hiro4669 <adfacer@yahoo.co.jp>
2014-01-19 10:24:17 +0900

    goを修正
    --HG--
    branch : go_debug

--- as/as1/as11c.c
+++ as/as1/as11c.c
@@ -4,32 +4,16 @@ char atmp2[];
 char atmp3[];
 
 char errflg;
-char *outbuf;
+/*char *outbuf;*/
+char outbuf[];
 char pof;
 char fbfil;
-char *symend;
+char *symend[];
 char usymtab[];
 
-char sdebug8[] " 000000\n";
-
-debug8(str, val) char *str; {
-	register int i, v;
-	register char *p;
-	for (i = 0; str[i]; ++i);
-	write(1, str, i);
-	v = val;
-	p = &sdebug8[7];
-	for (i = 0; i < 6; i++) {
-		*--p = '0' + (v & 7);
-		v =>> 3;
-	}
-	write(1, sdebug8, 8);
-}
 
 go()
 {
-	int i;  /* for debug */
-
 	int fp;
 	assem();
 	write(pof, outbuf, 512);
@@ -42,22 +26,12 @@ go()
 
 	fp = fcreat(atmp3);
 	
-	write(1, atmp3, 10);
-	write(1, "\n\n\n", 3);
-	write(1, fp + '0', 1);
-	write(1, "\n\n\n", 3);
-	debug8("symend", symend);
-	debug8("usymtab", usymtab);
-	debug8("symend - usymtab", symend - usymtab);
-	write(1, "\n\n\n", 3);
-
-	write(fp, usymtab, (symend - usymtab));
+	write(fp, usymtab, (*symend - usymtab));
 	close(fp);
 
-	/*
+
 	execl("/lib/as2", "/lib/as2", atmp1, atmp2, atmp3, "-g");
 	filerr("lib/as2", "?\n");
-	*/
 }
 
 
