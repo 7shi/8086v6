2f87bbd9021693c71fc6ac873ca766de5da3cf26
warabanshi <warabanshi@gmail.com>
2014-02-01 23:26:17 +0900

    convert rname() it returns error "158: true undefined" by doing "make test"

--- as/as1/as14c.c
+++ as/as1/as14c.c
@@ -1,7 +1,162 @@
 /* translated from as14.s */
 
 char ch;
-char symbol;
+char *symbol;
+int *symend;
+int *hshsiz;
+int *hshtab;
+int *end;
+char usymtab[36];
+char symtab[];
+
+rname(r0, r1, r2, r3)
+int r0;
+int r1;
+char *r2;
+char *r3;
+{
+    int r4, r5;
+    int *r0p, *r1p, *r4p;
+    int i, tmp1, tmp2;
+    int loop_flag, jump_flag;
+    int *sp;
+    char stack[10];
+
+    loop_flag = jump_flag = 0;
+    sp = &stack[9];
+
+    *sp = r1;
+    *(--sp) = r2;
+    *(--sp) = r3;
+
+    
+    r5 = 8;
+    r2 = symbol+8;
+    for (i = 0; i < 4; i++) {
+        *r2 = 0;
+        r2--;
+    }
+    *(--sp) = 0;
+    *(--sp) = 0;
+    
+    /* see later */
+    if (r0 == '~') {
+        sp =+ 2;
+        *sp =+ 1;
+    }
+
+    /*
+    retrieve source code by rch() while continuous normal characters
+    */
+    while (true) {
+        r3 = rch();
+        if (r3 <= 0) {
+            break;
+        } else {
+            tmp1 = tmp2 = *sp + r3;
+            tmp2 = tmp2 << 8;
+            tmp1 = tmp1 >> 8;
+            *sp = tmp1 | tmp2;
+
+            if (--r5 >= 0) {
+                *r2 = r3;
+                r2++;
+            }
+        }
+    }
+
+    ch = r0;
+    r1 = *(sp++);
+    r0 = 0;
+    
+    if (*(sp++) != 0) {
+        r4 = *symend;
+    } else {
+        r0 = r0 / hshsiz;
+        r1 = r0 % hshsiz;
+        r0 =* 2;
+        r1 =* 2;
+        r1p = hshtab + r1;
+
+        while (true) {
+            r1p =- r0;
+            if (r1p < hshtab) {
+                r1 =+ 2*(*hshsiz);
+            }
+
+            r2 = symbol;
+            r4p = *(--r1p);
+            if (r4p != 0) {
+                for (i = 0; i < 4; i++) {
+                    if (*(r2++) != *(r4p++)) {
+                        loop_flag = 1;
+                        break;
+                    }
+                }
+                if (loop_flag == 1) {
+                    continue;
+                } else {
+                    jump_flag = 1;
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        if (jump_flag == 0) {
+            /*3:*/
+            r4 = *symend;
+            *r1p = r4;
+        }
+    }
+
+    /*4:*/
+    if (jump_flag == 0) {
+        r2 = symbol;
+        *(--sp) = r4;
+        r4 =+ 20;
+
+        if (r4 > end) {
+            end =+ 512;
+        }
+        /*4:*/
+        r4p = *(sp++);
+        for (i = 0; i < 4; i++) {
+            *(r4p++) = *(r2++);
+        }
+        *(r4p++) = 0;
+        *(r4p++) = 0;
+
+        symend = *(r4p);
+        r4p =- 4;
+    }
+       
+    /* 1: */
+    *(--sp) = r4p;
+    r3 = r4p;
+    r3 =- 8;
+    if (r3 >= usymtab) {
+        r3 =- usymtab;
+        r2 = 0;
+        r2 = 3 / r3;
+        r3 = 3 % r3;
+        r4p = r2;
+        r4p =+ 4000;
+    } else {
+        r3 =- symtab;
+        r2 = 0;
+        r2 = 3 / r3;
+        r3 = 3 % r3;
+        r4p = r2;
+        r4p =+ 1000;
+    }
+
+    putw(r4p);
+
+    return r4p;
+}
+
 char inbuf[512];
 char fin;
 char fileflg;
