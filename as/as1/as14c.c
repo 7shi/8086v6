ca8ffb0bebe9459f33767979aacbfd23eb990292
7shi <7shi@live.jp>
2014-03-03 14:22:41 +0900

    [as1]補助関数をutils.cに分離

--- as/as1/as14c.c
+++ as/as1/as14c.c
@@ -1,34 +1,5 @@
 /* translated from as14.s */
 
-strncmp(s1, s2, n)
-char *s1, *s2;
-{
-    int d;
-    for (; n > 0; --n, ++s1, ++s2) {
-        d = *s1 - *s2;
-        if (d < 0) return -1;
-        if (d > 0) return 1;
-        if (!*s1) return 0;
-    }
-    return 0;
-}
-
-memcpy(d, s, n)
-char *d, *s;
-{
-    for (; n > 0; --n, ++d, ++s) {
-        *d = *s;
-    }
-}
-
-memset(d, v, n)
-char *d;
-{
-    for (; n > 0; --n, ++d) {
-        *d = v;
-    }
-}
-
 int  ch;
 char *symbol;
 int hshsiz 1553;
---
+++ as/as1/utils.c
@@ -0,0 +1,42 @@
+strncmp(s1, s2, n)
+char *s1, *s2;
+{
+	int d;
+	for (; n > 0; --n, ++s1, ++s2) {
+		d = *s1 - *s2;
+		if (d < 0) return -1;
+		if (d > 0) return 1;
+		if (!*s1) return 0;
+	}
+	return 0;
+}
+
+memcpy(d, s, n)
+char *d, *s;
+{
+	for (; n > 0; --n, ++d, ++s) {
+		*d = *s;
+	}
+}
+
+memset(d, v, n)
+char *d;
+{
+	for (; n > 0; --n, ++d) {
+		*d = v;
+	}
+}
+
+debug8(s, v)
+char *s; {
+	int i;
+	char buf[7];
+	for (i = 0; s[i]; ++i);
+	write(2, s, i);
+	for (i = 0; i < 6; ++i) {
+		buf[5 - i] = '0' + (v & 7);
+		v =>> 3;
+	}
+	buf[6] = '\n';
+	write(2, buf, 7);
+}
