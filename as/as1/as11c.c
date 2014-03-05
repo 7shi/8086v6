52460619ac86062871e0e86ffb08a225ba660168
7shi <7shi@live.jp>
2014-03-05 22:54:46 +0900

    as11: コードの整理

--- as/as1/as11c.c
+++ as/as1/as11c.c
@@ -5,7 +5,6 @@ char atmp1[], atmp2[], atmp3[];
 char errflg, pof, fbfil;
 char *usymtab, *symend, *memend;
 
-
 go()
 {
 	int fp;
@@ -19,16 +18,14 @@ go()
 		aexit(); 
 	}
 
+	/* シンボルテーブルをダンプ */
 	fp = fcreat(atmp3);
-	
 	write(fp, usymtab, symend - usymtab);
 	close(fp);
-
 
 	execl("/lib/as2", "/lib/as2", atmp1, atmp2, atmp3, "-g");
 	filerr("lib/as2", "?\n");
 }
-
 
 aexit()
 {
@@ -37,8 +34,6 @@ aexit()
 	unlink(atmp3);	
 	exit(errflg);
 }
-
-
 
 filerr(r0, r5)
 char *r0;
@@ -49,7 +44,6 @@ char *r5;
 	write(1, r0, i);
 	write(1, r5, 2);
 }
-
 
 fcreat(r4)
 char *r4;
