848a9517ff4bda72789d47b99525791db590dfba
7shi <7shi@live.jp>
2014-04-08 23:23:20 +0900

    as15: readop(), rsch(): リファクタリング * 変数名の変更 * コメントに対応する文字を追記
    --HG--
    branch : as1

--- as/as1/as15c.c
+++ as/as1/as15c.c
@@ -4,82 +4,83 @@ int savop, numval;
 char ch, chartab[];
 
 readop() {
-	int r0, oldr0, r1, r4;
-	if (r4 = savop) {
+	int c, num, type, ret;
+	if (savop) {
+		ret = savop;
 		savop = 0;
-		return r4;
+		return ret;
 	}
 	for (;;) {
-		r4 = rch(); /* 0-127 */
-		r1 = chartab[r4];
-		if (r1 == -12 /* garb */)
+		c = rch(); /* 0-127 */
+		type = chartab[c];
+		if (type == -12 /* garb: #>?@`{} etc */) {
 			error("g");
-		else if (r1 != -18)
+		} else if (type != -18 /* '\t', '\r', ' ' */) {
 			break;
+		}
 	}
-	switch (r1) {
-	case -22: /* fixor | */
-		r4 = 31;
+	switch (type) {
+	case -22: /* fixor: | */
+		ret = 31;
 		break;
-	case -20: /* escp \ */
-		switch (r4 = rch()) {
-		case '/': r4 = '/'; break; /* \/ avoid comment */
-		case '<': r4 = 29; break;
-		case '>': r4 = 30; break;
-		case '%': r4 = 31; break;
+	case -20: /* escp: \ */
+		switch (ret = rch()) {
+		case '/': ret = '/'; break; /* \/ avoid comment */
+		case '<': ret = 29; break;
+		case '>': ret = 30; break;
+		case '%': ret = 31; break;
 		}
 		break;
-	case -16: /* retread $%&... */
+	case -16: /* retread: !$%&()*+,-:=[]^ */
+	case  -2: /* retread: 4(EOT), 10'\n', 59';' */
+		ret = c;
 		break;
-	case -14: /* dquote " */
-		oldr0 = rsch(0);
-		r0    = rsch(0);
-		r0 =<< 8;
-		r0 =| oldr0;
-		r4 = 1;
+	case -14: /* dquote: " */
+		c = rsch(0);
+		num = (rsch(0) << 8) | c;
+		ret = 1;
 		break;
-	case -10: /* squote ' */
-		r0 = rsch(0);
-		r4 = 1;
+	case -10: /* squote: ' */
+		num = rsch(0);
+		ret = 1;
 		break;
-	case -6: /* skip / comment */
+	case -6: /* skip: / (comment) */
 		do {
-			r4 = rch();
-		} while (r4 != 4/*EOT*/ && r4 != '\n');
+			ret = rch();
+		} while (ret != 4/*EOT*/ && ret != '\n');
 		break;
-	case -2: /* retread */
-		break;
-	case 0: /* string < */
+	case 0: /* string: < */
 		putw('<');
-		for (numval = 0; (r0 = rsch(1)) >= 0; ++numval)
-			putw(r0 | 256);
+		for (numval = 0; (c = rsch(1)) >= 0; ++numval) {
+			putw(c | 256);
+		}
 		putw(-1);
 		return '<';
-	default: /* -18, -12, 自然数（ASCIIのまま） */
-		ch = r4;
-		if ('0' <= r1 && r1 <= '9') {
-			r4 = number(&r0);
+	default: /* ASCII */
+		ch = c;
+		if ('0' <= c && c <= '9') {
+			ret = number(&num);
 			break;
 		}
 		return rname();
 	}
-	putw(r4);
-	if (r4 == 1) {
-		/* default(数字のとき), squote, dquote */
-		putw(numval = r0);
+	putw(ret);
+	if (ret == 1) {
+		/* default(数字), squote, dquote */
+		putw(numval = num);
 	}
-	return r4;
+	return ret;
 }
 
 rsch(isstr)
 {
-	int r0;
-	r0 = rch();
-	if (r0 == 4/*EOT*/ || r0 == '\n') {
+	int c;
+	c = rch();
+	if (c == 4/*EOT*/ || c == '\n') {
 		error("<");
 		aexit();
-	} else if (r0 == '\\') {
-		switch (r0 = rch()) {
+	} else if (c == '\\') {
+		switch (c = rch()) {
 		case 'n': return 10;
 		case 't': return  9;
 		case 'e': return  4;
@@ -88,8 +89,8 @@ rsch(isstr)
 		case 'a': return  6;
 		case 'p': return 27;
 		}
-		return r0;
+	} else if (isstr && c == '>') {
+		return -1;
 	}
-	if (isstr && r0 == '>') return -1;
-	return r0;
+	return c;
 }
