54bdbcec0c93bcb2f8700816b2d0a76bba43588b
7shi <7shi@live.jp>
2014-04-10 07:01:26 +0900

    as17: コードの整理 * 変数名を変更 * インデントをスペース4文字に変更
    --HG--
    branch : as1

--- as/as1/as17c.c
+++ as/as1/as17c.c
@@ -6,145 +6,141 @@ struct { int type, val; };
 
 /* 0 - 96 | 97 - 106 | 107 - 127 */
 
-int* expres(r4, r2, r3) int r4, *r2, *r3;{
-  int r0, r1;
-  char opr;
+expres(op, r2, r3)
+int *r2, *r3;
+{
+    int type, val;
+    char opr;
 
-  opr = '+';
-  opfound = 0;
-  *r2 = 0;
-  *r3 = 1; 
+    opr = '+';
+    opfound = 0;
+    *r2 = 0;
+    *r3 = 1; 
 
-  /* sbrtn: */
-  for(;;){
-    if(r4 < 0 || 127 < r4){
-      r0 = r4->type;
-      r1 = r4->val;
-    }else if(r4 >= 107){ /* 0f-9f */
-      r0 = r4;
-      *r2 = *r3 = 0;
-    }else if(r4 >= 97){ /* 0b-9b */
-      r0  = curfbr[r4 - 97];
-      *r2 = curfb [r4 - 97];
-      if(*r2 < 0) error("f");
-    }else{
-      /* mov $esw1,r1 */
-      switch(r4){
-        case  29:
-        case  30:
-        case  31:
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-        case '&':
-        case '%':
-        case '^':
-        case '!':
-          /* binop: */
-          if(opr != '+') error("e");
-          opr = r4;
-          r4 = readop();
-          continue;
+    /* sbrtn: */
+    for (;;) {
+        if (op < 0 || 127 < op) {
+            type = op->type;
+            val  = op->val;
+        } else if (op >= 107) { /* 0f-9f */
+            type = op;
+            *r2 = *r3 = 0;
+        } else if (op >= 97) { /* 0b-9b */
+            type = curfbr[op - 97];
+            *r2  = curfb [op - 97];
+            if (*r2 < 0) error("f");
+        } else {
+            switch (op) {
+                case 29: /* \< */
+                case 30: /* \> */
+                case 31: /* |, \% */
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '&':
+                case '%':
+                case '^':
+                case '!':
+                    /* binop: */
+                    if (opr != '+') error("e");
+                    opr = op;
+                    op = readop();
+                    continue;
 
-        case '[':
-          /* brack: */
-          r1 = r2;
-          r0 = r3;
-          r4 = expres(readop(), &r1, &r0);
-          if(r4 != ']') error("f");
-          break;
+                case '[':
+                    /* brack: */
+                    val  = r2;
+                    type = r3;
+                    op = expres(readop(), &val, &type);
+                    if (op != ']') error("f");
+                    break;
 
-        case 1:
-          /* exnum: */
-          r1 = numval;
-          r0 = 1;
-          break;
+                case 1:
+                    /* exnum: */
+                    type = 1;
+                    val  = numval;
+                    break;
 
-        case 0:
-        default:
-          if(opfound == 0) error("e");
-          return r4; /* finish: */
-      }
+                default:
+                    if (opfound == 0) error("e");
+                    return op; /* finish: */
+            }
+        }
+
+        /* oprand: */
+        opfound =+ 1;
+
+        switch (opr) {
+            case '+': /* exadd: */
+                *r3 = combin(type, *r3, 0);
+                *r2 =+ val;
+                break;
+
+            case '-': /* exsub: */
+                *r3 = combin(type, *r3, 1);
+                *r2 =- val;
+                break;
+
+            case '*': /* exmul: */
+                *r3 = combin(type, *r3, 0);
+                *r2 =* val;
+                break;
+
+            case '/': /* exdiv: */
+                *r3 = combin(type, *r3, 0);
+                *r2 =/ val;
+                break;
+
+            case 31: /* exor: |, \% */
+                *r3 = combin(type, *r3, 0);
+                *r2 =| val;
+                break;
+
+            case '&': /* exand: */
+                *r3 = combin(type, *r3, 0);
+                *r2 =& val;
+                break;
+
+            case 29: /* exlsh: \< */
+                *r3 = combin(type, *r3, 0);
+                *r2 =<< val;
+                break;
+
+            case 30: /* exrsh: \> */
+                *r3 = combin(type, *r3, 0);
+                *r2 =>> val;
+                break;
+
+            case '%': /* exmod: */
+                *r3 = combin(type, *r3, 0);
+                *r2 =% val;
+                break;
+
+            case '!': /* exnot: */
+                *r3 = combin(type, *r3, 0);
+                *r2 =+ ~val;
+                break;
+
+            case '^': /* excmbin: */
+                *r3 = type; /* give left flag of right */
+                break;
+        }
+
+        opr = '+'; /* eoprnd: */
+        op = readop(); /* advanc: */
     }
-
-    /* oprand: */
-    opfound =+ 1;
-   
-    switch(opr){
-      case '+': /* exadd: */
-        *r3 = combin(r0, *r3, 0);
-        *r2 =+ r1;
-        break;
-
-      case '-': /* exsub: */
-        *r3 = combin(r0, *r3, 1);
-        *r2 =- r1;
-        break;
-
-      case '*': /* exmul: */
-        *r3 = combin(r0, *r3, 0);
-        *r2 =* r1;
-        break;
-
-      case '/': /* exdiv: */
-        *r3 = combin(r0, *r3, 0);
-        *r2 =/ r1;
-        break;
-
-      case  31: /* exor: */
-        *r3 = combin(r0, *r3, 0);
-        *r2 =| r1;
-        break;
-
-      case '&': /* exand: */
-        *r3 = combin(r0, *r3, 0);
-        *r2 =& r1;
-        break;
-
-      case  29: /* exlsh: */
-        *r3 = combin(r0, *r3, 0);
-        *r2 =<< r1;
-        break;
-
-      case  30: /* exrsh: */
-        *r3 = combin(r0, *r3, 0);
-        *r2 =>> r1;
-        break;
-
-      case '%': /* exmod: */
-        *r3 = combin(r0, *r3, 0);
-        *r2 =% r1;
-        break;
-
-      case '!': /* exnot: */
-        *r3 = combin(r0, *r3, 0);
-        *r2 =+ ~r1;
-        break;
-
-      case '^': /* excmbin: */
-        *r3 = r0; /* give left flag of right */
-        break;
-    }
-
-    opr = '+'; /* eoprnd: */
-    r4 = readop(); /* advanc: */
-  }
 }
 
- /* void _combin(r0:in, r3:out, r5:in)
-  *  value : r0, r3  (r3 == pc)
-  *  bool  : r5
-  */
-combin(r0, r3, r5) {
-  int v;
+combin(type, r3, r5) {
+    int v;
 
-  v  = 32 & (r0 | r3);
-  r0 = 31 & r0;
-  r3 = 31 & r3;
+    v  = (type | r3) & 32;
+    type =& 31;
+    r3 =& 31;
 
-  if(r0 == 0 || r3 == 0) return v;
-  if(r5 && r0 == r3) return v | 1;
-  if(r0 > r3) return v | r0;
-  return v | r3;
+    if (type == 0 || r3 == 0) return v;
+    if (r5 && type == r3) return v | 1;
+    if (type > r3) return v | type;
+    return v | r3;
 }
