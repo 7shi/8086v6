06aeeb3979b152c77f9d7ee5b554a98aa7212a6e
7shi <7shi@live.jp>
2014-04-10 22:58:29 +0900

    as17: expres(): ローカル変数xで擬似オブジェクト指向化
    --HG--
    branch : as1

--- as/as1/as17c.c
+++ as/as1/as17c.c
@@ -8,7 +8,8 @@ int curfb[], opfound, numval, savop;
 expres(this, op)
 struct Op *this;
 {
-    int type, value, isglobl;
+    struct Op x;
+    int isglobl;
     char opr;
 
     this->type  = 1; /* absolute */
@@ -20,14 +21,14 @@ struct Op *this;
     /* sbrtn: */
     for (;;) {
         if (op < 0 || 127 < op) {
-            type = op->type;
-            value  = op->value;
+            x.type  = op->type;
+            x.value = op->value;
         } else if ('a' <= op && op <= 'j') { /* 0b-9b */
-            type = curfbr[op - 97];
+            x.type = curfbr[op - 97];
             this->value  = curfb [op - 97];
             if (this->value < 0) error("f");
         } else if ('k' <= op && op <= 't') { /* 0f-9f */
-            type = op;
+            x.type = op;
             this->value = this->type = 0;
         } else {
             switch (op) {
@@ -47,12 +48,12 @@ struct Op *this;
                 op = readop();
                 continue;
             case '[':
-                value = expres(this, readop());
+                expres(&x, readop());
                 if ((op = readop()) != ']') error("f");
                 break;
             case 1:
-                type  = 1;
-                value = numval;
+                x.type  = 1;
+                x.value = numval;
                 break;
             default:
                 if (opfound == 0) error("e");
@@ -65,32 +66,32 @@ struct Op *this;
         opfound =+ 1;
         if (opr == '^') {
             /* give left flag of right */
-            this->type = type;
+            this->type = x.type;
         } else {
             /* combin: */
-            isglobl = (type | this->type) & 32;
-            type =& 31;
+            isglobl = (x.type | this->type) & 32;
+            x.type =& 31;
             this->type =& 31;
-            if (type == 0 || this->type == 0) {
+            if (x.type == 0 || this->type == 0) {
                 this->type = isglobl;
-            } else if (opr == '-' && type == this->type) {
+            } else if (opr == '-' && x.type == this->type) {
                 this->type = isglobl | 1;
-            } else if (type > this->type) {
-                this->type = isglobl | type;
+            } else if (x.type > this->type) {
+                this->type = isglobl | x.type;
             } else {
                 this->type = isglobl | this->type;
             }
             switch (opr) {
-            case '+': this->value =+  value; break;
-            case '-': this->value =-  value; break;
-            case '*': this->value =*  value; break;
-            case '/': this->value =/  value; break;
-            case  31: this->value =|  value; break; /* |, \% */
-            case '&': this->value =&  value; break;
-            case  29: this->value =<< value; break; /* \< */
-            case  30: this->value =>> value; break; /* \> */
-            case '%': this->value =%  value; break;
-            case '!': this->value =+ ~value; break;
+            case '+': this->value =+  x.value; break;
+            case '-': this->value =-  x.value; break;
+            case '*': this->value =*  x.value; break;
+            case '/': this->value =/  x.value; break;
+            case  31: this->value =|  x.value; break; /* |, \% */
+            case '&': this->value =&  x.value; break;
+            case  29: this->value =<< x.value; break; /* \< */
+            case  30: this->value =>> x.value; break; /* \> */
+            case '%': this->value =%  x.value; break;
+            case '!': this->value =+ ~x.value; break;
             }
         }
         opr = '+'; /* eoprnd: */
