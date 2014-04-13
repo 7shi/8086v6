b2fcec92b19bb08f1553f6a210fe1d8be6d9f9ae
7shi <7shi@live.jp>
2014-04-13 11:30:52 +0900

    as24: putw(), flush(): C言語化完了 * オリジナルの内容を削除してラッパー化

--- as/as2/as24.s
+++ as/as2/as24.s
@@ -11,66 +11,19 @@ oset:
 	rts r5
 
 putw:
-	mov	r1,-(sp)
-/	mov r0, -(sp)
-/	mov $'0, -(sp)
-/	jsr pc, _debug8
-/	tst (sp)+
-/	mov (sp)+, r0
-	mov	r2,-(sp)
-	mov	(r5)+,r2
-	mov	(r2)+,r1	/ slot
-	cmp	r1,(r2)		/ buf max
-	bhis	1f
-	mov	r0,(r1)+
-	mov	r1,-(r2)
-	br	2f
-1:
-	tst	(r2)+
-	mov	r0,-(sp)
-	jsr	r5,flush1
-	mov	(sp)+,r0
-	mov	r0,*(r2)+
-	add	$2,-(r2)
-2:
-	mov	(sp)+,r2
-	mov	(sp)+,r1
-	rts	r5
+	mov r1, -(sp)
+	mov (r5)+, -(sp)
+	mov r0, -(sp)
+	jsr pc, _putw
+	cmp (sp)+, (sp)+
+	mov (sp)+, r1
+	rts r5
 
 flush:
 	mov (r5)+, -(sp)
 	jsr pc, _flush
 	tst (sp)+
 	rts r5
-
-flush_:
-	mov	(r5)+,r2
-	cmp	(r2)+,(r2)+
-flush1:
-	mov	(r2)+,r1
-	mov	r1,0f		/ seek address
-	mov	fout,r0
-	sys	indir; 9f
-	.data
-9:	sys	seek; 0:..; 0
-	.text
-	bic	$!777,r1
-	mov	r2,r0
-	add	r0,r1		/ write address
-	mov	r1,0f
-	bis	$777,-(r2)
-	inc	(r2)		/ new seek addr
-	cmp	-(r2),-(r2)
-	sub	(r2),r1
-	neg	r1
-	mov	r1,0f+2		/ count
-	mov	r0,(r2)		/ new next slot
-	mov	fout,r0
-	sys	indir; 9f
-	.data
-9:	sys	write; 0:..; ..
-	.text
-	rts	r5
 
 readop:
 	mov r0, -(sp)
