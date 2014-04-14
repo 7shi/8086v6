788f5a3b9711dcf1c5d37480cf42f375bb0352ec
7shi <7shi@live.jp>
2014-04-14 22:09:54 +0900

    as23: assem(), checkeos(): C言語化（goto残置） * 不要なラッパーを削除

--- as/as2/as21.s
+++ as/as2/as21.s
@@ -2,16 +2,3 @@
 /
 
 / a21 -- pdp-11 assembler pass 2 
-
-.globl _assem
-_assem:
-	mov r2, -(sp)
-	mov r3, -(sp)
-	mov r4, -(sp)
-	mov r5, -(sp)
-	jsr pc, assem
-	mov (sp)+, r5
-	mov (sp)+, r4
-	mov (sp)+, r3
-	mov (sp)+, r2
-	rts pc
--- as/as2/as23c.c
+++ as/as2/as23c.c
@@ -1,6 +1,115 @@
 /* translated from as23.s */
 
-int curfb[], nxtfb[], *fbbufp;
+int line, savop, passno, dot, dotrel, brdelt, numval, txtsiz[];
+int curfb[];
+char symtab[];
+struct { char cval; };
+struct { int ival; };
+
+assem()
+{
+    int r0, r1, r2, r3, r4, sp;
+assem0:
+    r4 = readop();
+    if (r4 != 5 && r4 != '<') {
+        if (checkeos(r4)) goto ealoop;
+        sp = r4;
+        if (r4 == 1) {
+            sp = 2;
+            r4 = getw();
+            numval = r4;
+        }
+        r4 = readop();
+        if (r4 == '=') goto assem4;
+        if (r4 == ':') goto assem1;
+        savop = r4;
+        r4 = sp;
+    }
+    opline(r4);
+dotmax:
+    if (passno) goto ealoop;
+    r0 = dotrel;
+    if (txtsiz[r0 - 2] < dot) {
+        txtsiz[r0 - 2] = dot;
+    }
+    goto ealoop;
+assem1:
+    r4 = sp;
+    if (r4 < 128) {
+        if (r4 == 2) goto assem3;
+        error("x");
+        goto assem0;
+    }
+    if (!passno) {
+        r0 = r4->cval;
+        r0 =& 31;
+        if (r0 != 0 && r0 != 27 && r0 != 28) {
+            error("m");
+        }
+        r4->ival =& ~31;
+        r4->ival =| dotrel;
+        brdelt = (r4 + 2)->ival;
+        brdelt =- dot;
+        (r4 + 2)->ival = dot;
+        goto assem0;
+    }
+    if ((r4 + 2)->ival == dot) goto assem0;
+    error("p");
+    goto assem0;
+assem3:
+    r4 = numval;
+    fbadv(r4);
+    r0 = curfb[r4];
+    r0->cval = dotrel;
+    brdelt = (r0 + 2)->ival;
+    brdelt =- dot;
+    (r0 + 2)->ival = dot;
+    goto assem0;
+assem4:
+    r4 = readop();
+    r4 = expres(r4, &r2, &r3);
+    r1 = sp;
+    if (r1 == symtab) { /* test for dot */
+        r3 =& ~32;
+        if (r3 == dotrel) { /* can't change relocation */
+            if (r3 == 4) { /* bss */
+                dot = r2;
+                goto dotmax;
+            }
+            r2 =- dot;
+            if (r2 >= 0) {
+                sp = r2;
+                while (--sp >= 0) {
+                    outb(0, 1);
+                }
+                goto dotmax;
+            }
+        }
+        error(".");
+        goto ealoop;
+    }
+    if (r3 == 32) error("r");
+    r1->ival =& ~31;
+    r3 =& 31;
+    if (r3 == 0) r2 = 0;
+    r1->cval =| r3;
+    (r1 + 2)->ival = r2;
+
+ealoop:
+    if (r4 == '\n') {
+        ++line;
+        goto assem0;
+    } else if (r4 != 4/*EOT*/) {
+        goto assem0;
+    }
+}
+
+checkeos(r4)
+{
+    return r4 == '\n' || r4 == ';' || r4 == 4/*EOT*/;
+}
+
+int nxtfb[], *fbbufp;
 
 fbadv(r4)
 {
