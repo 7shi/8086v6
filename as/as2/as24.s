123de034e41450e9e1e1f15b9b6a5bd923cf0cac
7shi <7shi@live.jp>
2014-04-14 23:17:17 +0900

    as24: 不要なラッパーを削除

--- as/as2/as24.s
+++ as/as2/as24.s
@@ -10,15 +10,6 @@ oset:
 	cmp (sp)+, (sp)+
 	rts r5
 
-putw:
-	mov r1, -(sp)
-	mov (r5)+, -(sp)
-	mov r0, -(sp)
-	jsr pc, _putw
-	cmp (sp)+, (sp)+
-	mov (sp)+, r1
-	rts r5
-
 flush:
 	mov (r5)+, -(sp)
 	jsr pc, _flush
