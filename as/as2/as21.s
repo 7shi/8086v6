ced7e2f02bf00bfa998c811edc519aeb49702347
7shi <7shi@live.jp>
2014-04-14 22:06:06 +0900

    as21: 不要なラッパーを削除

--- as/as2/as21.s
+++ as/as2/as21.s
@@ -3,34 +3,6 @@
 
 / a21 -- pdp-11 assembler pass 2 
 
-aexit:
-	jmp _aexit
-
-filerr:
-	mov *(r5), -(sp)
-	jmp _filerr
-
-doreloc:
-	mov r1, -(sp)
-	jsr pc, _doreloc
-	mov (sp)+, r1
-	rts pc
-
-setbrk:
-	mov r1, -(sp)
-	jsr pc, _setbrk
-	mov (sp)+, r1
-	rts pc
-
-setup:
-	jmp _setup
-
-ofile:
-	mov *(r5)+, -(sp)
-	jsr pc, _ofile
-	tst (sp)+
-	rts r5
-
 .globl _assem
 _assem:
 	mov r2, -(sp)
