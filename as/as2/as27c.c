07c9be1e92e48bd8d336147cf69c2d16850a8473
7shi <7shi@live.jp>
2014-04-19 10:55:56 +0900

    as27: expres1(), combin(): 擬似オブジェクト指向化

--- as/as2/as27c.c
+++ as/as2/as27c.c
@@ -7,20 +7,25 @@ int xsymbol;
 expres(r4, r2, r3)
 int *r2, *r3;
 {
+    struct Op x;
     xsymbol = 0;
-    return expres1(r4, r2, r3);
+    r4 = expres1(&x, r4);
+    *r2 = x.value;
+    *r3 = x.type;
+    return r4;
 }
 
 int numval, esw1, passno, curfb[];
 char reltp2[], reltm2[], relte2[];
 
-expres1(r4, r2, r3)
-int *r2, *r3;
+expres1(this, r4)
+struct Op *this;
 {
+    struct Op x;
     int r0, r1, opr;
     opr = '+';
-    *r2 = 0;
-    *r3 = 1;
+    this->type  = 1;
+    this->value = 0;
     for (;;) {
         r0 = r4;
         if (r0 < 0 || 127 < r0) {
@@ -41,7 +46,9 @@ int *r2, *r3;
         } else {
             switch (r4) {
             case '[':
-                r4 = expres1(readop(), &r1, &r0);
+                r4 = expres1(&x, readop());
+                r0 = x.type;
+                r1 = x.value;
                 if (r4 != ']') error("]");
                 break;
             case   1:
@@ -74,47 +81,47 @@ int *r2, *r3;
 
         switch (opr) {
         case '^':
-            *r3 = r0;
+            this->type = r0;
             break;
         case  29:
-            *r3 = combin(r0, *r3, relte2);
-            *r2 =<< r1;
+            combin(this, r0, relte2);
+            this->value =<< r1;
             break;
         case  30:
-            *r3 = combin(r0, *r3, relte2);
-            *r2 =>> r1;
+            combin(this, r0, relte2);
+            this->value =>> r1;
             break;
         case  31:
-            *r3 = combin(r0, *r3, relte2);
-            *r2 =| r1;
+            combin(this, r0, relte2);
+            this->value =| r1;
             break;
         case '+':
-            *r3 = combin(r0, *r3, reltp2);
-            *r2 =+ r1;
+            combin(this, r0, reltp2);
+            this->value =+ r1;
             break;
         case '-':
-            *r3 = combin(r0, *r3, reltm2);
-            *r2 =- r1;
+            combin(this, r0, reltm2);
+            this->value =- r1;
             break;
         case '*':
-            *r3 = combin(r0, *r3, relte2);
-            *r2 =* r1;
+            combin(this, r0, relte2);
+            this->value =* r1;
             break;
         case '/':
-            *r3 = combin(r0, *r3, relte2);
-            *r2 =/ r1;
+            combin(this, r0, relte2);
+            this->value =/ r1;
             break;
         case '&':
-            *r3 = combin(r0, *r3, relte2);
-            *r2 =& r1;
+            combin(this, r0, relte2);
+            this->value =& r1;
             break;
         case '%':
-            *r3 = combin(r0, *r3, relte2);
-            *r2 =% r1;
+            combin(this, r0, relte2);
+            this->value =% r1;
             break;
         case '!':
-            *r3 = combin(r0, *r3, relte2);
-            *r2 =+ ~r1;
+            combin(this, r0, relte2);
+            this->value =+ ~r1;
             break;
         }
         opr = '+';
@@ -124,35 +131,35 @@ int *r2, *r3;
 
 int maxtyp;
 
-combin(r0, r3, r5)
+combin(this, r0, r5)
+struct Op *this;
 char *r5;
 {
     int r1, globl, tmp;
     if (!passno) {
-        globl = (r0 | r3) & 32;
+        globl = (r0 | this->type) & 32;
         r0 =& 31;
-        r3 =& 31;
-        if (r0 > r3) {
+        this->type =& 31;
+        if (r0 > this->type) {
             tmp = r0;
-            r0 = r3;
-            r3 = tmp;
+            r0 = this->type;
+            this->type = tmp;
         }
         if (!r0) {
-            r3 = 0;
-        } else if (r5 == reltm2 && r0 == r3) {
-            r3 = 1;
+            this->type = 0;
+        } else if (r5 == reltm2 && r0 == this->type) {
+            this->type = 1;
         }
-        r3 =| globl;
+        this->type =| globl;
     } else {
         maxtyp = 0;
         r1 = maprel(r0) * 6;
-        r3 = r5[maprel(r3) + r1];
-        if (r3 < 0) {
-            if (r3 != -1) error("r");
-            r3 = maxtyp;
+        this->type = r5[maprel(this->type) + r1];
+        if (this->type < 0) {
+            if (this->type != -1) error("r");
+            this->type = maxtyp;
         }
     }
-    return r3;
 }
 
 int maxtyp;
