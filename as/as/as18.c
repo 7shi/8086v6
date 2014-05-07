396ce1b2fee4096239dd0ed47480e5b289a474a1
7shi <7shi@live.jp>
2014-05-07 14:26:43 +0900

    as1+2: 削除: as28.c * as18.cに統合

--- as/as/as18.c
+++ as/as/as18.c
@@ -55,3 +55,39 @@ char **curarg;
 
 char fileflg, ch;
 int line, errflg, ifflg, savop, numval;
+
+/* translated from as28.s */
+
+char *aout "a.out";
+
+int header[8];
+int *txtmagic &header[0];
+int *txtsiz   &header[1];
+int *datsiz   &header[2];
+int *bsssiz   &header[3];
+int *symsiz   &header[4];
+int *stksiz   &header[5];
+int *exorig   &header[6];
+
+int tseeks[3], rseeks[3], symseek;
+
+int *txtseek &tseeks[0];
+int *datseek &tseeks[1];
+
+int *trelseek &rseeks[0];
+int *drelseek &rseeks[1];
+
+int brlen 1024;
+char brtab[128]; /* bitmap: brlen / 8 */
+
+int adrbuf[6];
+int brtabi, brdelt, defund, datbase, bssbase;
+int abufi, ibufi, ibufc, passno;
+
+char argb[22], *txtp[259], *relp[259], faout;
+char *usymtab, *endtable, *xsymbol;
+
+struct Op { char type, num; int value; };
+
+/* 0: .. 9: */
+struct Op curfbr[10], *curfb[10], *nxtfb[10], *fbbufp;
